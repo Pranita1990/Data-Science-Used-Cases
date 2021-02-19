739ca8a849df0b6420f636f42ae1e696d48cf2ae
Pranita1990 <66178539+Pranita1990@users.noreply.github.com>
2021-02-19 11:17:10 -0800

    Add files via upload

---
+++ Air Quality Index Check Data Collection #2.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def avg_data_2013():
+    temp_i=0
+    average=[]
+    for rows in pd.read_csv(r'Users/analasa10/Data/AQI/aqi2013.csv',chunksize=24):
+        add_var=0
+        avg=0.0
+        data=[]
+        df=pd.DataFrame(data=rows)
+        for index,row in df.iterrows():
+            data.append(rows['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var=add_var+i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='Invld':
+                    temp=float(i)
+                    add_var=add_var+temp
+        avg=add_var/24
+        temp_i=temp_i+1
+
+        average.append(avg) 
+        return average
+
+def avg_data_2014():
+    temp_i=0
+    average=[]
+    for rows in pd.read_csv(r'Users/analasa10/Data/AQI/aqi2014.csv',chunksize=24):
+        add_var=0
+        avg=0.0
+        data=[]
+        df=pd.DataFrame(data=rows)
+        for index,row in df.iterrows():
+            data.append(rows['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var=add_var+i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='Invld':
+                    temp=float(i)
+                    add_var=add_var+temp
+                    avg=add_var/24
+        temp_i=temp_i+1
+
+    average.append(avg)   
+    return average          
+
+def avg_data_2015():
+    temp_i=0
+    average=[]
+    for rows in pd.read_csv(r'Users/analasa10/Data/AQI/2015.csv',chunksize=24):
+        add_var=0
+        avg=0.0
+        data=[]
+        df=pd.DataFrame(data=rows)
+        for index,row in df.iterrows():
+            data.append(rows['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var=add_var+i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='Invld':
+                    temp=float(i)
+                    add_var=add_var+temp
+                    avg=add_var/24
+        temp_i=temp_i+1
+         
+    average.append(avg)   
+    return average
+
+def avg_data_2016():
+    temp_i=0
+    average=[]
+    for rows in pd.read_csv(r'Users/analasa10/Data/AQI/aqi2016.csv',chunksize=24):
+        add_var=0
+        avg=0.0
+        data=[]
+        df=pd.DataFrame(data=rows)
+        for index,row in df.iterrows():
+            data.append(rows['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var=add_var+i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='Invld':
+                    temp=float(i)
+                    add_var=add_var+temp
+                avg=add_var/24
+        temp_i=temp_i+1
+    average.append(avg)   
+    return average
+     
+def avg_data_2017():
+    temp_i=0
+    average=[]
+    for rows in pd.read_csv(r'Users/analasa10/Data/AQI/aqi2017.csv',chunksize=24):
+        add_var=0
+        avg=0.0
+        data=[]
+        df=pd.DataFrame(data=rows)
+        for index,row in df.iterrows():
+            data.append(rows['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var=add_var+i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='Invld':
+                    temp=float(i)
+                    add_var=add_var+temp
+                avg=add_var/24
+        temp_i=temp_i+1
+    average.append(avg)   
+    return average        
+
+def avg_data_2018():
+    temp_i=0
+    average=[]
+    for rows in pd.read_csv(r'Users/analasa10/Data/AQI/aqi2018.csv',chunksize=24):
+        add_var=0
+        avg=0.0
+        data=[]
+        df=pd.DataFrame(data=rows)
+        for index,row in df.iterrows():
+            data.append(rows['PM2.5'])
+        for i in data:
+            if type(i) is float or type(i) is int:
+                add_var=add_var+i
+            elif type(i) is str:
+                if i!='NoData' and i!='PwrFail' and i!='---' and i!='Invld':
+                    temp=float(i)
+                    add_var=add_var+temp
+                avg=add_var/24
+        temp_i=temp_i+1
+    average.append(avg)   
+    return average
+     
+        
+        
+if __name__=="__main__":
+    lst2013=avg_data_2013()
+    lst2014=avg_data_2014()
+    lst2015=avg_data_2015()
+    lst2016=avg_data_2016()
+    lst2017=avg_data_2017()
+    lst2018=avg_data_2018()
+    plt.plot(range(0,365),label="2013 data")
+    plt.plot(range(0,364),label="2014 data")
+    plt.plot(range(0,365),label="2015 data")
+    
